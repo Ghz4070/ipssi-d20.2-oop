543416c5a0e2a959ca966a177c0666ea2f24d2bd
Ghz4070 <ghezal-ilies@outlook.fr>
2018-12-17 10:20:54 +0100

    Modif

---
+++ src/Entity/Participant.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Connect4\Entity;
+
+use Support\Entity\Participant as ParticipantInterface;
+
+interface Participant extends ParticipantInterface
+{
+
+}
---
+++ src/Entity/Piece.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Checkers\Entity;
+
+final class Piece
+{
+    private const BLUE = 'bleu';
+    private const RED = 'rouge';
+    private $color;
+
+    private function __construct(string $color)
+    {
+        $this->color = $color;
+    }
+
+    public function __toString(): string
+    {
+        return $this->color;
+    }
+
+    public static function createWhite(): Piece
+    {
+        return new self(self::bleu);
+    }
+
+    public static function createBlack(): Piece
+    {
+        return new self(self::rouge);
+    }
+}
